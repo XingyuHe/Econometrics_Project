baf5c3b3822d1a66db278ba252fae0fb8a76a31c
Xingyu (Peter) He <xhe@mac-21043.hamilton.edu>
2018-11-11 01:08:57 -0500

    change IP address

--- changeIP.py
+++ changeIP.py
@@ -1,8 +1,5 @@
-import requests
+import os
 
-proxies = {
-  'http': 'http://10.10.1.10:3128',
-  'https': 'http://10.10.1.10:1080',
-}
+ip = "192.168.33.2"
 
-requests.get('https://trends.google.com/trends/explore?date=all_2008&geo=US&gprop=news&q=free%20trade', proxies=proxies)
+os.system("networksetup -setmanualwithdhcprouter Wi-Fi "+str(ip))
